ec7125ba1fbb6c19a55345e2d667e8fd05e20773
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-03-06 23:30:18 -0500

    1 file modified

--- loop.js
+++ loop.js
@@ -1,25 +1,25 @@
 // for(initialization;codition;increment/decrement)
 // it will start from one till it reaches 10
-for (var a = 1; a <= 10; a++) {
-  // switch (x) {
-  //   case 1:
-  //     document.write('One');
-  //     break;
+for (var x = 1; x <= 5; x++) {
+  switch (x) {
+    case 1:
+      document.write('One');
+      break;
 
-  //   case 2:
-  //     document.write('Two');
-  //     break;
+    case 2:
+      document.write('Two');
+      break;
 
-  //   case 3:
-  //     document.write('Three');
-  //     break;
+    case 3:
+      document.write('Three');
+      break;
 
-  //   case 4:
-  //     document.write('Four');
-  //     break;
-  //   case 5:
-  //     document.write('Five');
-  //     break;
-  // }
-  document.write('<h1>' + 10 * a + '</h1>');
+    case 4:
+      document.write('Four');
+      break;
+    case 5:
+      document.write('Five');
+      break;
+  }
+  // document.write('<h1>' + 10 * x + '</h1>');
 }
