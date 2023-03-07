e3216d2d3794ff8fd99c4fe94acf201be60a382f
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-03-06 23:19:01 -0500

    1 file modified

--- for-loop-javascript.js
+++ for-loop-javascript.js
@@ -1,5 +1,25 @@
 // for(initialization;codition;increment/decrement)
 // it will start from one till it reaches 10
-for (var x = 1; x <= 10; a++) {
-  
+for (var x = 1; x <= 5; a++) {
+  var y = 1;
+  switch (y) {
+    case 1:
+      document.write("One")
+      break;
+
+      case 1:
+        document.write("One")
+        break;
+
+        case 1:
+          document.write("One")
+          break;
+
+          case 1:
+            document.write("One")
+            break;
+            case 1:
+              document.write("One")
+              break;
+  }
 }
